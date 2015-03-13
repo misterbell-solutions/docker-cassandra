cd4fd8f28cfcf5264a1bc2d99bc3393f2b3c2d66
Christophe Billiard <cbilliard@misterbell.com>
2015-03-13 16:27:43 +0100

    removing unnecessary customisation

--- cassandra-cluster/scripts/cassandra-clusternode.sh
+++ cassandra-cluster/scripts/cassandra-clusternode.sh
@@ -3,14 +3,6 @@
 # Get running container's IP
 IP=`hostname --ip-address`
 
-# Setup cluster name
-if [ -z "$CASSANDRA_CLUSTERNAME" ]; then
-        echo "No cluster name specified, preserving default one"
-else
-        sed -i -e "s/^cluster_name:.*/cluster_name: $CASSANDRA_CLUSTERNAME/" $CASSANDRA_CONFIG/cassandra.yaml
-fi
-
-
 # Dunno why zeroes here
 sed -i -e "s/^rpc_address.*/rpc_address: $IP/" $CASSANDRA_CONFIG/cassandra.yaml
 
@@ -31,9 +23,6 @@ if [ -z "$CASSANDRA_SEEDS" ]; then
 	CASSANDRA_SEEDS=$SEEDS
 fi
 sed -i -e "s/- seeds: \"127.0.0.1\"/- seeds: \"$CASSANDRA_SEEDS\"/" $CASSANDRA_CONFIG/cassandra.yaml
-
-# Most likely not needed
-echo "JVM_OPTS=\"\$JVM_OPTS -Djava.rmi.server.hostname=$IP\"" >> $CASSANDRA_CONFIG/cassandra-env.sh
 
 echo "Starting Cassandra on $IP..."
 
