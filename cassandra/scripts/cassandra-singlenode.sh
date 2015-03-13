e436c34f5c25bec6b4fee230d1738998709eb0c6
Christophe Billiard <cbilliard@misterbell.com>
2015-03-13 11:31:16 +0100

    changing listening address from container's IP to HOST IP

--- cassandra/scripts/cassandra-singlenode.sh
+++ cassandra/scripts/cassandra-singlenode.sh
@@ -14,7 +14,7 @@ sed -i -e "s/^rpc_address.*/rpc_address: 0.0.0.0/" $CASSANDRA_CONFIG/cassandra.y
 sed -i -e "s/- seeds: \"127.0.0.1\"/- seeds: \"$SEEDS\"/" $CASSANDRA_CONFIG/cassandra.yaml
 
 # Listen on IP:port of the container
-sed -i -e "s/^listen_address.*/listen_address: $IP/" $CASSANDRA_CONFIG/cassandra.yaml
+sed -i -e "s/^listen_address.*/listen_address: $HOST/" $CASSANDRA_CONFIG/cassandra.yaml
 
 # Broadcast on IP:port of the container
 sed -i -e "s/^# broadcast_address.*/broadcast_address: $HOST/" $CASSANDRA_CONFIG/cassandra.yaml
